4f561b1728f66296eadf82ae2a00cc2b510e8709
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2021-02-24 19:19:12 -0500

    slide show running

--- script.js
+++ script.js
@@ -5,8 +5,37 @@ const slideshowDivs = () =>{
 
         div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg`
 
+        // When i equals add the class of 'change' to the div
+        i === 1 && div.classList.add('change')
+
         document.querySelector('.slideshow').appendChild(div)
     }
 }
 slideshowDivs()
+
+const divs = document.querySelectorAll('.slideshow div')
+
+let a = 1
+
+const slideshow = () => {
+    setInterval(()=>{
+        a++
+
+        const div = document.querySelector('.slideshow .change')
+
+
+        div.classList.remove('change')
+
+        if(a < divs.length){
+            div.nextElementSibling.classList.add('change')
+        }else{
+            divs[0].classList.add('change')
+            a = 1
+        }
+        
+    }, 1000)
+}
+
+
+slideshow()
 // End of Slideshow
